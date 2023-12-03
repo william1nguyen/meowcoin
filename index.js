7e89367eb930cee371020c2c90ead81b75c94566
natalieconan <natalieconan17@gmail.com>
2023-12-04 01:21:49 +0700

    add multiple transactions and mining reward

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
-const SHA256 = require('crypto-js/sha256');
+ const SHA256 = require('crypto-js/sha256');
+
+class Transaction {
+    constructor(fromAddress, toAddress, amount) {
+        this.fromAddress = fromAddress;
+        this.toAddress = toAddress;
+        this.amount = amount;
+    }
+}
 
 class Block {
-    constructor(index, timestamp, data, previousHash = '') {
-        this.index = index;
+    constructor(timestamp, transactions, previousHash = '') {
         this.timestamp = timestamp;
-        this.data = data;
+        this.transactions = transactions;
         this.previousHash = previousHash;
         this.hash = this.caculateHash();
         this.nonce = 0; // changed in order to change hash value
@@ -20,7 +27,7 @@ class Block {
         ).toString();
     }
 
-    mineBlock(difficulty) {
+    mineBlock(difficulty) { // for security purposes
         while(this.hash.startsWith("0".repeat(difficulty))) {
             this.nonce ++;
             this.hash = this.caculateHash();
@@ -33,21 +40,56 @@ class Block {
 class BlockChain {
     constructor() {
         this.chain = [ this.createGenesisBlock() ];
-        this.difficulty = 10;
+        this.difficulty = 2;
+        this.pendingTransactions = []
+        this.miningReward = 100;
     }
 
     createGenesisBlock() {
-        return new Block(0, "02/12/2023", { name: "Genesis block" }, "0");
+        const block = new Block(Date.now(), [], "0");
+        return block;
     }
 
     getLastestBlock() {
         return this.chain[this.chain.length - 1];
     }
 
-    addBlock(newBlock) {
-        newBlock.previousHash = this.getLastestBlock().hash;
-        newBlock.mineBlock(this.difficulty);
-        this.chain.push(newBlock);
+    minePendingTransaction(miningRewaredAddress) {
+        let block = new Block(Date.now(), this.pendingTransactions);
+        block.mineBlock(this.difficulty);
+
+        console.log("This block is mined!...");
+        this.chain.push(block);
+
+        // receive reward
+        this.pendingTransactions = [
+            new Transaction(null, miningRewaredAddress, this.miningReward)
+        ];
+    }
+
+    createTransaction(transaction) {
+        this.pendingTransactions.push(transaction);
+    }
+
+    getBalanceOfAddress(address) {
+        let balance = 0;
+
+        for (const block of this.chain) {
+            for (const trans of block.transactions) {
+                if (address === 'vanhpew') {
+                    console.log(trans);
+                }
+                if (trans.fromAddress === address) {
+                    balance -= trans.amount;
+                }
+                
+                if (trans.toAddress === address) {
+                    balance += trans.amount;
+                }
+            }
+        }
+
+        return balance;
     }
 
     isChainValid() {
@@ -67,32 +109,3 @@ class BlockChain {
         return true;
     }
 }
-
-const pewCoin = new BlockChain();
-
-console.log("Mining block 1...");
-pewCoin.addBlock(new Block(
-    index=1,
-    timestamp="5/12/2023",
-    data={
-        name: "transaction",
-        from: "A",
-        to: "B",
-        amount: "100$"
-    }
-));
-
-console.log("Mining block 2...");
-pewCoin.addBlock(new Block(
-    index=1,
-    timestamp="7/12/2023",
-    data={
-        name: "transaction",
-        from: "A",
-        to: "B",
-        amount: "500$"
-    }
-));
-
-console.log("PewCoin: ", JSON.stringify(pewCoin, null, 4));
-console.log('Is blockchain valid ?: ', pewCoin.isChainValid());
